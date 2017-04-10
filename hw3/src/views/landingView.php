534eddbd6a3a72c3f9df998e36a60a0eff428ed8
dewdee <mikeyng93@gmail.com>
2017-04-10 00:08:14 -0700

    Fixed weird list offsetting

--- hw3/src/views/landingView.php
+++ hw3/src/views/landingView.php
@@ -10,50 +10,61 @@ class landingView extends View {
         <h1><a href="index.php">Note-A-List
             </a>
         </h1>
-
-        <table>
-            <tr>
-                <th>
-                    <h2>Lists</h2>
-                </th>
-                <th colspan="2">
-                    <h2>Notes</h2>
-                </th>
-            </tr>
-            <tr>
-                <td><ul>
-                        <?php
+        <div>
+            <table>
+                <tr>
+                    <th>
+                        <h2>Lists</h2>
+                    </th>
+                </tr>
+                <tr>
+                    <td><ul>
+                            <?php
                             $prev = isset($_REQUEST['listName']) ? $_REQUEST['listName'] : '';
-                        ?>
-                        <li><a href="index.php?c=listController&m=newList&previousList=<?=urlencode($prev)?>">[New List]</a></li>
-                        <?php
-                        if(!empty($data['lists'])){
-                            foreach($data['lists'] as $name){
-                                ?>
-                                <li><a href="index.php?c=listController&m=selectList&previousList=<?=urlencode($prev)?>&listName=<?=urlencode($name)
-                                    ?>"><?=$name?></a></li>
-                                <?php
+                            ?>
+                            <li><a href="index.php?c=listController&m=newList&previousList=<?=urlencode($prev)?>">[New List]</a></li>
+                            <?php
+                            if(!empty($data['lists'])){
+                                foreach($data['lists'] as $name){
+                                    ?>
+                                    <li><a href="index.php?c=listController&m=selectList&previousList=<?=urlencode($prev)?>&listName=<?=urlencode($name)
+                                        ?>"><?=$name?></a></li>
+                                    <?php
+                                }
                             }
-                        }
-                        ?>
-                    </ul>
-                </td>
-                <td><ul>
-                        <li><a href="index.php?c=noteController&m=newNote&previousList=<?=urlencode($prev)?>">[New Note]</a></li>
-                        <?php
-                        if(!empty($data['notes'])){
-                            foreach($data['notes'] as $name => $created){
-                                ?>
-                                <li><a href="index.php?c=noteController&m=selectNote&previousList=<?=urlencode($prev)?>&noteName=<?=urlencode($name)
-                                    ?>"><?=$name?></a> <?=$created?></li>
-                                <?php
+                            ?>
+                        </ul>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div>
+            <table>
+                <tr>
+                    <th colspan="2">
+                        <h2>Notes</h2>
+                    </th>
+                </tr>
+                <tr>
+                    <td><ul>
+                            <li><a href="index.php?c=noteController&m=newNote&previousList=<?=urlencode($prev)?>">[New Note]</a></li>
+                            <?php
+                            if(!empty($data['notes'])){
+                                foreach($data['notes'] as $name => $created){
+                                    ?>
+                                    <li><a href="index.php?c=noteController&m=selectNote&previousList=<?=urlencode($prev)?>&noteName=<?=urlencode($name)
+                                        ?>"><?=$name?></a> <?=$created?></li>
+                                    <?php
+                                }
                             }
-                        }
-                        ?>
-                    </ul>
-                </td>
-            </tr>
-        </table>
+                            ?>
+                        </ul>
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+
         <?php
     }
 }
