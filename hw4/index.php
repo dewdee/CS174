3a5823adc5b44a2dc56f0dc2b8bf87ee313b6fa8
dewdee <mikeyng93@gmail.com>
2017-04-23 22:41:45 -0700

    Added some things to index, created api main and sheet controller

--- hw4/index.php
+++ hw4/index.php
@@ -2,6 +2,8 @@
 
 namespace mn\hw4;
 
+require_once 'src/configs/config.php';
+
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\FirePHPHandler;
@@ -12,8 +14,27 @@ $loader->add('AppName', __DIR__.'/../src/');
 // Create the logger
 $logger = new Logger('my_logger');
 // Now add some handlers
-$logger->pushHandler(new StreamHandler(__DIR__.'/my_app.log', Logger::DEBUG));
+$logger->pushHandler(new StreamHandler(__DIR__.'/app_data/spread.log', Logger::DEBUG));
 $logger->pushHandler(new FirePHPHandler());
 
 // You can now use your logger
 $logger->info('My logger is now ready');
+
+if(!isset($_REQUEST['c'])) {
+    header("Location: index.php?c=main&m=view");
+}
+else{
+    if($_REQUEST['c'] == "sheet"){
+        if(isset($_REQUEST['m'])){
+            switch($_REQUEST['m']){
+                case "edit":
+                    echo 'edit';
+                    break;
+                case "read":
+                    echo 'read';
+                    break;
+
+            }
+        }
+    }
+}
---
+++ hw4/src/controllers/MainController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace mn\hw4\controllers;
+
+require_once 'Controller.php';
+
+class MainController extends Controller {
+    public function __construct() {
+        parent::__construct();
+    }
+}
