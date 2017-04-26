7ac66135b0e895ebd10717d6f88a9320dcc29310
dewdee <mikeyng93@gmail.com>
2017-04-25 23:53:40 -0700

    Modified sheet controller to create read and edit views

--- hw4/src/controllers/SheetController.php
+++ hw4/src/controllers/SheetController.php
@@ -4,8 +4,24 @@ namespace mn\hw4\controllers;
 
 require_once 'Controller.php';
 
-class SheetController extends Controller {
-    public function __construct() {
+use mn\hw4\views\editView;
+use mn\hw4\views\readView;
+
+class sheetController extends Controller {
+    public function __construct($type) {
         parent::__construct();
+        switch($type){
+            case "edit":
+                $this->view = new editView("editLayout");
+                break;
+            case "read":
+                $this->view = new readView("readLayout");
+                break;
+        }
+    }
+
+    public function index(){
+        $data['sheetName'] = $_REQUEST['arg1'];
+        $this->view->display($data);
     }
 }
