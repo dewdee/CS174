04b0f96c176a068ad5117a0a0aae4f6f5a79553f
dewdee <mikeyng93@gmail.com>
2017-04-27 08:25:09 -0700

    Began work on readView

--- hw4/src/views/readView.php
+++ hw4/src/views/readView.php
@@ -10,6 +10,13 @@ class readView extends View{
         $logger->info('Visited read page');
     }
     public function render($data = []){
+        $codes = array_values($data['sheetCodes']);
+        $url = [];
+        foreach($codes as $code){
+            //index.php?c=main&m=edit&arg1=8_digit_hash_e
+            $str = 'index.php?c=main&m=edit&arg1='.$code[1].'_'.$code[2];
+            array_push($url, $str);
+        }
         ?>
             <h1><a href="index.php">Web Sheets : <?=$data['sheetName']?></a></h1>
             <label for="fileURL">File URL:</label>
