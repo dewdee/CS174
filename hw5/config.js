5d11c12b8d5730a17d3b4585546a68e598616f7b
dewdee <mikeyng93@gmail.com>
2017-05-14 09:59:28 -0700

    Added mySQL module

--- hw5/config.js
+++ hw5/config.js
@@ -1,3 +1,10 @@
+/*
+Your Config.js file should also have three configurable properties: 
+check_in_frequency, which controls how frequently users need to check in, measured in milliseconds
+notify_delay, which determines the minimum time in milliseconds after a failed check-in before a user's Let-Know List is emailed,
+email_job_frequency, which controls how often the callback to send out a batch of emails is called.
+*/
+
 var config = {
     "foo": "foo",
     "goo": "goo"
