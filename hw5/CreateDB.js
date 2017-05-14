b3d16d9a58b6c63a423f297774da978dfc8fb85e
dewdee <mikeyng93@gmail.com>
2017-05-14 10:07:09 -0700

    CreateDB Finished

---
+++ hw5/CreateDB.js
@@ -0,0 +1,32 @@
+/*
+The grader can use the line node CreateDB.js to create your mysql database.
+The database has a table USER(ID, EMAIL, LAST_CHECK_IN, LAST_EMAIL_SENT, NOTIFY_LIST, MESSAGE).
+LAST_CHECK_IN, LAST_EMAIL_SENT are Unix timestamps which are initially 0.
+*/
+
+var mysql = require('mysql')
+    // set-up and connect
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password'
+        // could also add a database property
+});
+connection.connect();
+connection.query('CREATE DATABASE IF NOT EXISTS hw5_mn',
+    function(error, results, fields) {
+        if (error) throw error;
+    }
+);
+connection.query('USE hw5_mn', function(error, results, fields) {
+    if (error) throw error;
+});
+connection.query('CREATE TABLE IF NOT EXISTS USER(ID INTEGER, EMAIL TEXT, LAST_CHECK_IN TIMESTAMP, LAST_EMAIL_SENT TIMESTAMP, NOTIFY_LIST TEXT, MESSAGE TEXT)',
+    function(error, results, fields) {
+        if (error) throw error;
+    }
+);
+/* close the connection.
+   makes sure all queries terminate before closing connection
+ */
+connection.end(function(err) {});
